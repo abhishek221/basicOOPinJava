3e3b1c67798ab663dcefdabd219667d3defa5ee5
ABHISHEK <abhishek.yadav_cs18@gla.ac.in>
2019-09-04 15:51:24 +0530

    Initial commit

---
+++ src/lab04/Main.java
@@ -0,0 +1,9 @@
+package lab04;
+
+public class Main {
+    public static void main(String[] args) {
+        Person obj = new Person("rohan",19,'M');
+        System.out.println(obj.toString());
+
+    }
+}
